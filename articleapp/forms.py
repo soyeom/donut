f31e0861b1dca6d9bdc95091ef276601b6f7bdf3
soyeom <92167007+soyeom@users.noreply.github.com>
2022-03-01 01:01:06 +0900

    Revert "Jae won"

--- articleapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from articleapp.models import Article
-
-
-class ArticleCreationForm(ModelForm):
-    class Meta:
-        model = Article
-        fields = ['title', 'content', 'mainphoto']
