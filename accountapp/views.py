ad94279532e8f7e0a79ffded27a92bc5bf6c44a1
seolhaaa <shchu07@korea.ac.kr>
2022-02-28 19:42:56 +0900

    accountapp : bug fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -29,15 +29,15 @@ def donut(request):
             return HttpResponseRedirect(reverse('accountapp:hello_world'))
         else:
             donut_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+            return render(request, 'accountapp/donut.html', context={'donut_list': donut_list})
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:donut')
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView, MultipleObjectMixin):
+class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
@@ -48,7 +48,7 @@ class AccountUpdateView(CreateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:donut')
     template_name = 'accountapp/update.html'
 
 @method_decorator(has_ownership, 'get')
