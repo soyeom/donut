ce14577bc052654e3e7ea22975ddd6cc52b202e9
seolhaaa <shchu07@korea.ac.kr>
2022-02-28 13:27:00 +0900

    fix bug

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+#from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
