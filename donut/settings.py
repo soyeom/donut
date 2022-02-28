61679f9a23c947e9121b70c2710e70204f5f758b
seolhaaa <shchu07@korea.ac.kr>
2022-03-01 00:10:56 +0900

    apply bootstrap

--- donut/settings.py
+++ donut/settings.py
@@ -54,9 +54,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp',
     'articleapp',
-    'bootstrap4',
 ]
 
 MIDDLEWARE = [
