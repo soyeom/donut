8d53a9998432a66dd20111e20911c63f01d7bf04
seolhaaa <shchu07@korea.ac.kr>
2022-02-28 13:13:56 +0900

    accountapp : create, delete, detail, update view

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,5 +1,68 @@
-from django.db import models
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
-# Create your models here.
-class HelloWorld(models.Model):
-    text = models.CharField(max_length=225, null=False)
+# Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
+
+from accountapp.decorators import account_ownership_required
+from accountapp.forms import AccountUpdateForm
+from accountapp.models import HelloWorld
+from articleapp.models import Article
+
+has_ownership = [account_ownership_required, login_required]
+
+@login_required
+def hello_world(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')
+
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView, MultipleObjectMixin):
+    model = User
+    context_object_name = 'target_user' # 내 계정 말고 해당 계정 정보를 보기 위한
+    template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(CreateView):
+    model = User
+    context_object_name = 'target_user'
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
