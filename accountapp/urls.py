d4e36d9db1c63e7e6374442021c28eebb1a004f0
ShinDodam <tlseheka@naver.com>
2022-02-28 22:12:19 +0900

    create.html modify

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
 from accountapp.views import donut, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+
 app_name = 'accountapp'
 
 urlpatterns = [
-
     path('donut/', donut, name='donut'),
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
