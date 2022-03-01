c2fd7bc0b7d41b6ccb9b56988c9e8c7fb0c90be0
seolhaaa <shchu07@korea.ac.kr>
2022-03-01 12:05:55 +0900

    introapp 2

--- introapp/urls.py
+++ introapp/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path
+from django.views.generic import TemplateView
 
 app_name = 'introapp'
 
--- introapp/views.py
+++ introapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
 
 class IntroListView(ListView):
-    model = Intro
     context_object_name = 'intro_list'
     template_name = 'introapp/list.html'
     paginate_by = 3
