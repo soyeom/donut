c20bac04a9ba8c9222135aea33390e58cd65cccf
ShinDodam <tlseheka@naver.com>
2022-03-01 20:23:11 +0900

    last distrbution

--- donut/urls.py
+++ donut/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
-
-
+from introapp.views import IntroListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
