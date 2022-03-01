0e45348028f4f9b3785cb7213eae5be049b9935c
Your Name <you@example.com>
2022-03-01 20:52:30 +0900

    last commit

--- introapp/views.py
+++ introapp/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import ListView
 from .models import Image
 
@@ -9,8 +6,3 @@ class IntroListView(ListView):
     context_object_name = 'intro_list'
     template_name = 'introapp/list.html'
     paginate_by = 3
-
-
-
-
-
