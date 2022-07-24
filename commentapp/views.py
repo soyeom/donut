ad7827a34cdc2d230855fd0e8447430989f007bd
DESKTOP-KSQCDNA\jimflower <whsndk2@korea.ac.kr>
2022-07-24 18:20:51 +0900

    last

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import CreateView, DeleteView
-
+from django.utils.decorators import method_decorator
 from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
-
+from commentapp.decorators import comment_ownership_required
 
 class CommentCreateView(CreateView):
     model = Comment
@@ -23,7 +23,8 @@ class CommentCreateView(CreateView):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk })
 
 
-
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
 class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
