20eb095ee56dca45bf4f10bcd909263616e436c9
DESKTOP-KSQCDNA\jimflower <whsndk2@korea.ac.kr>
2022-07-24 18:09:00 +0900

    last

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
@@ -18,6 +18,16 @@ class CommentCreateView(CreateView):
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk })
+
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk })
