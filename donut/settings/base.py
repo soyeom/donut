8b0302152a3ce168383997cf8be698c78fa76d11
Your Name <you@example.com>
2022-07-24 15:37:30 +0900

    sass setting

--- donut/settings/base.py
+++ donut/settings/base.py
@@ -27,11 +27,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sass_processor',
     'accountapp',
     'articleapp',
     'bootstrap4',
     'introapp',
 ]
+
+SASS_PROCESSOR_ENABLED =True
+SASS_PROCESSOR_ROOT =os.path.join(BASE_DIR,'static')
+SASS_OUTPUT_STYLE = 'compact'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
