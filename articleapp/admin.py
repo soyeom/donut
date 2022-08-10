4fa5b6625ab0021954ed4ccce68a30c1565d68db
DESKTOP-KSQCDNA\jimflower <whsndk2@korea.ac.kr>
2022-08-11 00:38:45 +0900

    kanghyeon

--- articleapp/admin.py
+++ articleapp/admin.py
@@ -3,10 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from articleapp.models import Campaign
+from articleapp.models import Campaign, Article
 
 
 @admin.register(Campaign)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['Participants', 'title', 'amount','price','state']
+    list_display = ['Participants', 'title', 'amount', 'price', 'state']
+    list_display_links = ['state']
+
+
+@admin.register(Article)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'amount', 'price', 'state']
     list_display_links = ['state']
