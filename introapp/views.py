7566197fcd0e2e048bcb144b2b8319c24ec43248
Your Name <you@example.com>
2022-08-18 23:02:41 +0900

    campaign_title complete

--- introapp/views.py
+++ introapp/views.py
@@ -15,7 +15,10 @@ class IntroListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['popular_article'] = Article.objects.order_by('-hit')[:5]
-        context['campaign'] = Campaign.objects.filter(participants_id=self.request.user.id)
+        campaign = Campaign.objects.filter(participants_id=self.request.user.id, state__in='abc').values('article_id')
+        print(campaign[0]['article_id'])
+        article = Article.objects.get(id=campaign[0]['article_id'])
+        context['campaign_title'] = article.title
         return context
 
 def societyinfo(request):
