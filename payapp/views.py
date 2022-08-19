e9253a3d286dc56aeab763c39501e9688fbcdde0
Your Name <you@example.com>
2022-08-19 17:35:29 +0900

    change merge

--- payapp/views.py
+++ payapp/views.py
@@ -24,8 +24,8 @@ def index(request):
             "partner_user_id": "german",    # 유저 아이디
             "item_name": article.title,        # 구매 물품 이름
             "quantity": "1",                # 구매 물품 수량
-            "total_amount": "100",        # 구매 물품 가격
-            "tax_free_amount": str(campaign.amount),         # 구매 물품 비과세
+            "total_amount": str(campaign.amount),        # 구매 물품 가격
+            "tax_free_amount": "0",         # 구매 물품 비과세
             "approval_url": "http://127.0.0.1:8000/pay/approval/",
             "cancel_url": "https://naver.com",
             "fail_url": "https://naver.com",
