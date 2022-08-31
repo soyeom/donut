46b401b91669dcc9a103730c28b4e9ef21634a41
Your Name <you@example.com>
2022-08-31 23:20:16 +0900

    change model

--- introapp/models.py
+++ introapp/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Society(models.Model):
+    society = models.TextField(max_length=50, null=True) # 기부단체 이름
+    region = models.TextField(max_length=20, null=True) # 읍면동
+    latitude = models.FloatField(default=0, null=True) # 위도
+    longitude = models.CharField(max_length=20) # 경도
+    content = models.TextField(null=True) # 상세내용
