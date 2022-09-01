b139cf758a8ab80b2b0bce6ae09afe3f2987d0e4
Your Name <you@example.com>
2022-09-01 20:05:34 +0900

    hidden checkbox

--- introapp/admin.py
+++ introapp/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
+
+from introapp.models import Society
+
+
+@admin.register(Society)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['society', 'region', 'latitude', 'longitude', 'content']
