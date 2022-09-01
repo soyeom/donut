7d41f85bb86d9080784856cc93d78096ce8bf9ec
Your Name <you@example.com>
2022-09-01 20:34:18 +0900

    merge with k

--- introapp/admin.py
+++ introapp/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from introapp.models import Society
+from .models import Society
 
 
 @admin.register(Society)
-class PostAdmin(admin.ModelAdmin):
+class Societyadmin(admin.ModelAdmin):
     list_display = ['society', 'region', 'latitude', 'longitude', 'content']
