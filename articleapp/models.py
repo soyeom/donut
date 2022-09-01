19b96a6bedb7371bba831117a16533d3534e7bf3
Your Name <you@example.com>
2022-09-01 23:30:54 +0900

    merge with d

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=20)
+
 class Article(models.Model):
+    category = models.ForeignKey(ArticleCategory, on_delete=models.CASCADE, related_name='articlecategory')
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True, blank=True)
