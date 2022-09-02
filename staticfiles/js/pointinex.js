6f02566f9c7171839419d34a70fe9b67ccf20b46
경민 <skmin3735@gmail.com>
2022-09-03 02:54:09 +0900

    kyeongmin

--- staticfiles/js/pointinex.js
+++
@@ -1,8 +0,0 @@
-function checkOnlyOne(element) {
-    const checkboxes
-        = document.getElementsByName("price");
-    checkboxes.forEach((cb) => {
-        cb.checked = false;
-    });
-    element.checked = true;
-}
