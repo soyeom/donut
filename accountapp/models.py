94956e9ddb31493a4ea23823a044361cdd1e896a
경민 <skmin3735@gmail.com>
2022-11-04 22:47:42 +0900

    kyeongmin

--- accountapp/models.py
+++ accountapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class User(AbstractUser):
     id = models.CharField(max_length=11, unique=True, primary_key=True)
+    username = models.CharField(max_length=30, unique=False)
+    USERNAME_FIELD = 'email'
     grade = models.ForeignKey('Grade', models.SET_NULL, blank=True, null=True)
 
 class Grade(models.Model):
