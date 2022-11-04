29b7a10d273de3274c0da031380b2722db4eab6b
Your Name <you@example.com>
2022-11-04 22:47:22 +0900

    add user_id field

--- accountapp/models.py
+++ accountapp/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    id = models.CharField(max_length=11, unique=True, primary_key=True)
+    user_id = models.CharField(max_length=11, unique=True)
     grade = models.ForeignKey('Grade', models.SET_NULL, blank=True, null=True)
 
 class Grade(models.Model):
