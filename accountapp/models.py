e8a2af207480c8bd4ec568852617329ddc212220
Your Name <you@example.com>
2022-11-13 16:28:30 +0900

    merge

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    id = models.CharField(max_length=11, primary_key=True)
+    user_id = models.CharField(max_length=11)
     grade = models.ForeignKey('Grade', models.SET_NULL, blank=True, null=True)
+
 
 class Grade(models.Model):
     grade = models.IntegerField(default='1')
